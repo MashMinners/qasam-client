2fe3ba648ce1dd721e3dc9f34d1bd763b8b5605a
MashMinners <Valehtelit@mail.ru>
2023-03-11 19:54:25 +1000

    #Mobile version. Закончил финальный шаг с таймером. теперь идет отсчет и по истечению сбрасывает на первую страницу

--- src/views/StageFinish.vue
+++ src/views/StageFinish.vue
@@ -3,7 +3,7 @@
     <div class="p-fluid grid">
       <div class="centered">
         <prime-message severity="info" :closable="false"> Благодарим за вашу оценку!</prime-message>
-        <prime-knob class="centered-text mt-1 mb-1" v-model="finalTimerCount" :max="10"/>
+        <prime-knob class="centered-text mt-1 mb-1" v-model="timerCount" :max="10"/>
         <prime-button label="Начать с начала" class="p-button-lg" @click="start"/>
       </div>
     </div>
@@ -14,21 +14,56 @@
 import {mapMutations} from "vuex";
 
 export default {
+  data() {
+    return {
+      timerEnabled: false,
+      timerCount: 10
+    }
+  },
   name: "StepFinish",
   methods: {
     ...mapMutations({
       clearModel: "app/CLEAR_MODEL"
     }),
     start(){
-      //this.pauseFinalTimer()
-      //this.pauseVotedTimer()
+      this.pauseTimer()
       setTimeout(() => {
-        this.finalTimerCount = 10;
-        this.votedTimerCount = 10;
+        this.timerCount = 10;
       }, 500);
       this.clearModel();
       this.$router.push({name: 'start'});
-    }
+    },
+    playTimer() {
+      this.timerEnabled = true;
+    },
+    pauseTimer() {
+      this.timerEnabled = false;
+    },
+  },
+  watch: {
+    //Синий таймер
+    timerEnabled(value) {
+      if (value) {
+        setTimeout(() => {
+          this.timerCount--;
+        }, 1000);
+      }
+    },
+    timerCount: {
+      handler(value) {
+        if (value > 0 && this.timerEnabled) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 1000);
+        }else if(value === 0){
+          this.start()
+        }
+      },
+      immediate: true // This ensures the watcher is triggered upon creation
+    },
+  },
+  mounted(){
+    this.playTimer()
   }
 }
 </script>
