08f865c047b393c2ca0742862aae1e765b711724
MashMinners <Valehtelit@mail.ru>
2023-03-11 19:37:59 +1000

    #Mobile version. Аннотация к шагу сканирования

--- src/views/StageScanning.vue
+++ src/views/StageScanning.vue
@@ -3,6 +3,7 @@
       <div class="card">
         <div class="p-fluid grid">
           <div class="centered">
+            <prime-message severity="info" :closable="false">Держите QR код на расстоянии 10-15 сантиметров от окна</prime-message>
             <prime-input-text id="autofocus" :value="dtoRecordId" @input="Vote" />
           </div>
         </div>
